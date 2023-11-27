bbf117bd5777df566b23908048b8a2ff68ff0384
Bogdan-Teodora <teodorabogdan2002@gmail.com>
2023-11-27 10:50:57 +0200

    process production

--- Lab5 - Week8 - Part 1/src/Grammar.java
+++ Lab5 - Week8 - Part 1/src/Grammar.java
@@ -20,6 +20,54 @@ public class Grammar {
     private String startingSymbol;
     private boolean isCFG;
 
+    /**
+     * With this method we first:
+     * -> Split the production by the left right hand side separator ("->")
+     * -> Then we split the left hand side by space (we can have A B -> something)
+     * -> Then we split the right hand side by "|".
+     * -> We go through each production from the right hand side and format each of the in order to be added to the map
+     * @param production -> represents the production we are about to work
+     */
+    private void processProduction(String production){
+        String[] leftAndRightHandSide = production.split(this.SEPARATOR_LEFT_RIGHT_HAND_SIDE);
+        List<String> splitLHS = List.of(leftAndRightHandSide[0].split(this.TRANSITION_CONCATENATION));
+        String[] splitRHS = leftAndRightHandSide[1].split(this.SEPARATOR_OR_TRANSITION);
+
+        this.productions.putIfAbsent(splitLHS, new HashSet<>());
+        for(int i=0; i<splitRHS.length; i++){
+            this.productions.get(splitLHS).add(Arrays.stream(splitRHS[i].split(this.TRANSITION_CONCATENATION)).collect(Collectors.toList()));
+        }
+    }
+
+    /**
+     * With this method we check if a grammar is a context free grammar
+     * -> First we check if the starting symbols is found within the non-terminals
+     * -> Second we check if on the left hand side we have only one non-terminal (for each production)
+     * -> Third we check if the productions of that left hand side non-terminal can be found within the non-terminals set or terminals set or is equal to the empty sequence
+     * @return true if the grammar is a CFG, false otherwise
+     */
+    private boolean checkIfCFG() {
+        if (!this.nonTerminals.contains(this.startingSymbol)) {
+            return false;
+        }
+
+        for (List<String> leftHandSide : this.productions.keySet()) {
+            // On the left hand side we need to have only one element (A -> a, not AB -> a, where A and B are different non-terminals)
+            if (leftHandSide.size() != 1 || !this.nonTerminals.contains(leftHandSide.get(0))) {
+                return false;
+            }
+
+            for (List<String> possibleNextMoves : this.productions.get(leftHandSide)) {
+                for (String possibleNextMove : possibleNextMoves) {
+                    if (!this.nonTerminals.contains(possibleNextMove) && !this.terminals.contains(possibleNextMove) && !possibleNextMove.equals(this.EPSILON)) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
 
     /**
      * With this method we load the content from the file (we read every line from the file and
@@ -33,7 +81,11 @@ public class Grammar {
             this.startingSymbol = scanner.nextLine();
 
             this.productions = new HashMap<>();
+            while (scanner.hasNextLine()) {
+                this.processProduction(scanner.nextLine());
+            }
 
+            this.isCFG = this.checkIfCFG();
         }
         catch (FileNotFoundException e) {
             e.printStackTrace();
