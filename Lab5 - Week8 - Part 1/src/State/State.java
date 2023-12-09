4291155019eb4a0242006efaaeafeb384faec4db
Bogdan-Teodora <teodorabogdan2002@gmail.com>
2023-12-09 18:49:08 +0200

    added the method setActionForState

--- Lab5 - Week8 - Part 1/src/State/State.java
+++ Lab5 - Week8 - Part 1/src/State/State.java
@@ -1,5 +1,7 @@
 package State;
 
+import LR.Grammar;
+
 import java.util.*;
 
 /**
@@ -7,6 +9,7 @@ import java.util.*;
  */
 public class State {
 
+    private StateActionType stateActionType;
     private Set<Item> items;
 
 
@@ -32,6 +35,25 @@ public class State {
         }
 
         return new ArrayList<>(symbols);
+    }
+
+    /**
+     * With this method we set the action for the state.
+     * The action can be: SHIFT, REDUCE, ACCEPT, REDUCE_REDUCE_CONFLICT or SHIFT_REDUCE_CONFLICT
+     */
+    public void setActionForState(){
+        if(items.size() == 1 && ((Item)items.toArray()[0]).getRightHandSide().size() == ((Item)items.toArray()[0]).getPositionForDot() && ((Item)this.items.toArray()[0]).getLeftHandSide() == Grammar.enrichedStartingGrammarSymbol){
+            this.stateActionType = StateActionType.ACCEPT;
+        } else if(items.size() == 1 && ((Item) items.toArray()[0]).getRightHandSide().size() == ((Item) items.toArray()[0]).getPositionForDot())
+        {
+            this.stateActionType = StateActionType.REDUCE;
+        } else if(items.size() >= 1 && this.items.stream().allMatch(i -> i.getRightHandSide().size() > i.getPositionForDot())){
+            this.stateActionType = StateActionType.SHIFT;
+        } else if(items.size() > 1 && this.items.stream().allMatch(i -> i.getRightHandSide().size() == i.getPositionForDot())){
+            this.stateActionType = StateActionType.REDUCE_REDUCE_CONFLICT;
+        } else {
+            this.stateActionType = StateActionType.SHIFT_REDUCE_CONFLICT;
+        }
     }
 
     @Override
